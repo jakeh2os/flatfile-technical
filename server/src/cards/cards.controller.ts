400b573ebbdf014c62497acf014012e68b8f60b5
Jake Waters <50031507+jakeh2os@users.noreply.github.com>
2024-03-10 14:44:52 -0600

    controller & service files updated
    fix: npm nodes installed
    feat: controller and service card files updates to be able to move cards

--- server/src/cards/cards.controller.ts
+++ server/src/cards/cards.controller.ts
@@ -1,17 +1,24 @@
-import { Body, Controller, Logger, Post } from '@nestjs/common'
-import { CardEntity } from '../entities/Card'
-import { CardsService } from './cards.service'
+import { Body, Controller, Logger, Post, Put, Param } from '@nestjs/common'; // Import Param decorator
+import { CardEntity } from '../entities/Card';
+import { CardsService } from './cards.service';
 
 @Controller('cards')
 export class CardsController {
-  private readonly logger = new Logger(CardsController.name)
+  private readonly logger = new Logger(CardsController.name);
 
   constructor(private cardsService: CardsService) {}
 
   @Post()
   addCard(@Body() card: { sectionId: number; title: string }): Promise<CardEntity> {
-    this.logger.log('POST /cards')
+    this.logger.log('POST /cards');
+    return this.cardsService.create(card);
+  }
 
-    return this.cardsService.create(card)
+  @Put(':id/move')
+  moveCard(@Body() moveData: { sectionId: number }, @Param('id') cardId: number): Promise<CardEntity> {
+    this.logger.log(`PUT /cards/${cardId}/move`);
+
+    return this.cardsService.move(cardId, moveData.sectionId);
   }
 }
+
--- server/src/cards/cards.service.ts
+++ server/src/cards/cards.service.ts
@@ -1,19 +1,28 @@
-import { Injectable } from '@nestjs/common'
-import { InjectRepository } from '@nestjs/typeorm'
-import { CardEntity } from '../entities/Card'
-import { Repository } from 'typeorm'
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CardEntity } from '../entities/Card';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CardsService {
   constructor(
     @InjectRepository(CardEntity)
-    private cardsRepository: Repository<CardEntity>
+    private cardsRepository: Repository<CardEntity>,
   ) {}
 
-  create({ sectionId, title }: { sectionId: number; title: string }): Promise<CardEntity> {
-    let card = new CardEntity()
-    card.title = title
-    card.section_id = sectionId
-    return this.cardsRepository.save(card)
+  async create({ sectionId, title }: { sectionId: number; title: string }): Promise<CardEntity> {
+    let card = new CardEntity();
+    card.title = title;
+    card.section_id = sectionId;
+    return this.cardsRepository.save(card);
+  }
+
+  async move(cardId: number, newSectionId: number): Promise<CardEntity> {
+    const card = await this.cardsRepository.findOne(cardId);
+    if (!card) {
+      throw new NotFoundException(`Card with id ${cardId} not found`);
+    }
+    card.section_id = newSectionId;
+    return this.cardsRepository.save(card);
   }
 }
